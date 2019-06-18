b21161ef96e294c90a3d43c75353dedc3eb662d2
deliciousmonster <jaxon@deliciousmonster.com>
2019-06-18 09:36:10 -0600

    update packages, update readme

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # UI Scaffold with PK Chooser
+
+## Getting Started
+
+Install dependencies:
+
+```npm i -s```
+
+Copy and rename the config file template:
+
+```cp config.example.js config.js```
+
+Start the web server
+
+```npm start```
+
+## Using a static Pubkeeper Server
+
+Once your system is created and you're ready to move the UI into the public domain, you'll want to remove the Pubkeeper Server chooser and replace it with your system's production Pubkeeper details.
+
+Open `config.js`, change `staticPubkeeper` to `true`, and enter your pubkeeper server details below. As well, we'd appreciate it if you removed the webAuth section from the auth0 section.
+
+## Using your own Auth0 account for authentication
+
+If you'd still like to use auth0 for webauth, you can create a free auth0 account [here](https://auth0.com/signup)
+
+Once you have an auth0 account, create a new application, copy your credentials, open `config.js`, and update the webAuth section appropriately.
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Card, CardBody, Row, Col } from '@nio/ui-kit';
+import { Card, CardBody, Clock, Row, Col, Button } from '@nio/ui-kit';
 
 export default class Page extends React.Component {
+  state = { currentTime: new Date(), historicalTime: [], brewedTime: new Date(), historicalBrewedTime: [] };
+
   componentDidMount = () => {
     const { pkClient } = this.props;
-    pkClient.addPatron('*.*', patron => patron.on('message', this.writeDataToState));
+    pkClient.addBrewer('ui_scaffold.example_brew', brewer => setInterval(() => brewer.brewJSON([{ time: new Date() }]), 1000));
+    pkClient.addPatron('ui_scaffold.example_brew', patron => patron.on('message', this.writeDataToState));
+    pkClient.addBrewer('ui_scaffold.example_brew2', brewer => this.brewer = brewer);
+    pkClient.addPatron('ui_scaffold.example_brew2', patron => patron.on('message', this.writeDataToState2));
   };
 
   componentWillUnmount = () => {
@@ -13,40 +18,95 @@ export default class Page extends React.Component {
   };
 
   writeDataToState = (data) => {
+    const { historicalTime } = this.state;
     const json = new TextDecoder().decode(data);
-    const newData = Array.isArray(JSON.parse(json)) ? JSON.parse(json)[0] : JSON.parse(json);
-    this.setState(newData);
-    console.log(newData);
+    const { time } = Array.isArray(JSON.parse(json)) ? JSON.parse(json)[0] : JSON.parse(json);
+    const currentTime = new Date(time);
+    historicalTime.unshift(currentTime.toLocaleString());
+    historicalTime.splice(6, 1);
+    this.setState({ currentTime, historicalTime });
+  };
+
+  writeDataToState2 = (data) => {
+    const { historicalBrewedTime } = this.state;
+    const json = new TextDecoder().decode(data);
+    const { newBrewedTime } = Array.isArray(JSON.parse(json)) ? JSON.parse(json)[0] : JSON.parse(json);
+    const brewedTime = new Date(newBrewedTime);
+    historicalBrewedTime.unshift(brewedTime.toLocaleString());
+    historicalBrewedTime.splice(4, 1);
+    this.setState({ brewedTime, historicalBrewedTime });
+  };
+
+  brewCurrentTimestamp = () => {
+    this.brewer.brewJSON([{ newBrewedTime: new Date() }]);
   };
 
   render = () => {
+    const { currentTime, historicalTime, brewedTime, historicalBrewedTime } = this.state;
+
     return (
-      <Row>
-        <Col xs="4" className="text-center">
-          <Card>
-            <CardBody className="p-3">
-              <h5 className="m-0">Topic 1</h5>
+      <Card>
+        <CardBody className="p-3">
+          <h2 className="m-0">UI Scaffold / Pubkeeper Demo</h2>
+          Sending signals to and receiving signals from nio services using the Pubkeeper javascript client.
+          <hr />
+          <Row>
+            <Col md="4">
+              <b>The Pubkeeper Provider is a HoC that uses React&apos;s Context API to:</b>
+              <ul>
+                <li>Connect to the Pubkeeper Server</li>
+                <li>Publish the time every second to <i>ui_scaffold.example_brew</i></li>
+                <li>Inject the connected pkClient via the <i>withPubkeeper</i> method</li>
+              </ul>
+            </Col>
+            <Col xs="12" className="d-inline-block d-md-none">
               <hr />
-            </CardBody>
-          </Card>
-        </Col>
-        <Col xs="4" className="text-center">
-          <Card>
-            <CardBody className="p-3">
-              <h5 className="m-0">Topic 2</h5>
+            </Col>
+            <Col md="4">
+              <b>The left side uses the pkClient to:</b>
+              <ul>
+                <li>Create a Patron of <i>ui_scaffold.example_brew</i></li>
+                <li>Assign inbound signals on that topic to an event handler <i>writeDataToState</i></li>
+                <li>Update the Clock and historical array based on updated local state</li>
+              </ul>
+            </Col>
+            <Col xs="12" className="d-inline-block d-md-none">
               <hr />
-            </CardBody>
-          </Card>
-        </Col>
-        <Col xs="4" className="text-center">
-          <Card>
-            <CardBody className="p-3">
-              <h5 className="m-0">Topic 3</h5>
+            </Col>
+            <Col md="4">
+              <b>The right side uses the pkClient to:</b>
+              <ul>
+                <li>Create a new Brewer <i>and</i> new Patron for topic <i>ui_scaffold.example_brew2</i></li>
+                <li>Brew the current time when you click the button</li>
+                <li>Assign inbound signals on that topic to an event handler <i>writeDataToState2</i></li>
+              </ul>
+            </Col>
+          </Row>
+          <hr className="my-3" />
+          <Row>
+            <Col md="3" sm="6" className="text-center mb-3 text-nowrap">
+              <Clock value={currentTime} />
+            </Col>
+            <Col md="3" sm="6">
+              <div id="demo_output">
+                {historicalTime && historicalTime.map(h => (<div key={h}>{h}</div>))}
+              </div>
+            </Col>
+            <Col xs="12" className="d-inline-block d-md-none">
               <hr />
-            </CardBody>
-          </Card>
-        </Col>
-      </Row>
+            </Col>
+            <Col md="3" sm="6" className="text-center mb-3 text-nowrap">
+              <Clock value={brewedTime} />
+            </Col>
+            <Col md="3" sm="6">
+              <Button block color="primary" onClick={() => this.brewCurrentTimestamp()} className="text-nowrap">Brew Current Time</Button>
+              <div id="demo_output_2">
+                {historicalBrewedTime && historicalBrewedTime.map((h, i) => (<div key={i}>{h}</div>))}
+              </div>
+            </Col>
+          </Row>
+        </CardBody>
+      </Card>
     );
   };
 }
